86b3a9aa3b4fa54aadabb4b90ae6dd6ccd8b1a26
tdhenry <henry@translatedigital.com>
2019-01-17 11:13:29 +1300

    change port number to 8080

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 80
+const port = 8080
 app.use(express.static('static'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
