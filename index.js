7275f0d0fd91fd15e22570525356d07695b59276
tdhenry <henry@translatedigital.com>
2019-01-17 10:47:13 +1300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = 80
+app.use(express.static('static'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
